6cc2d9e703e95d186e2535bead9ccc6abe17d645
unknown <luonggthanhcong@gmail.com>
2022-09-20 15:16:45 +0700

    add time to table

--- crawl.php
+++ crawl.php
@@ -7,16 +7,19 @@ $sql = "CREATE TABLE `lich_thi_va_ket_qua` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `bai_thi` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `ngay_thi` date DEFAULT NULL,
-    `gio_thi` varchar(200),
+    `gio_thi_ca_sang` time DEFAULT NULL,
+    `gio_thi_ca_chieu` time DEFAULT NULL,
+    `gio_thi_ca_toi` time DEFAULT NULL,
     `dia_diem_thi` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `ngay_tra_ket_qua` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `status` int(1) DEFAULT NULL,
     `updated_at` datetime DEFAULT NULL,
     `created_at` datetime DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `key2` (`bai_thi`,`ngay_thi`,`gio_thi`,`dia_diem_thi`)
+    UNIQUE KEY `key2` (`bai_thi`,`ngay_thi`,`gio_thi_ca_sang`,`gio_thi_ca_chieu`,`dia_diem_thi`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 if (mysqli_query($conn, $sql) === TRUE) {
-    echo "create table lich_thi_va_ket_qua";
+    // echo "create table lich_thi_va_ket_qua";
 } else {
     echo "can't create lich_thi_va_ket_qua <br>" . mysqli_error($conn);
 }
@@ -26,27 +29,97 @@ $default = $html->find('._list_location', 0);
 $content = $html->find('._list_location', 0)->find('tr');
 $list_result = array();
 foreach ($content as $key => $item) {
+
     if ($key == 0) {
         continue;
     }
+    $gio_thi = explode(" ", trim($item->childNodes(3)->plaintext));
+    $ca_sang = 0;
+    $ca_chieu = 0;
+    $ca_toi = 0;
+    if (count($gio_thi) == 1) {
+        // echo 'case=1';
+        if (strpos($gio_thi[0], 'AM') !== false) {
+            $ca_sang = "7:00";
+        }
+        if (strpos($gio_thi[0], 'PM') !== false) {
+            $ca_chieu = "13:00";
+        }
+        if (strpos($gio_thi[0], 'EV') !== false) {
+            $ca_toi = "18:00";
+        }
+        if (str_replace(':', '', $gio_thi[0]) > 1) {
+            if (str_replace(':', '', $gio_thi[0]) < 1300) {
+                $ca_sang = $gio_thi[0];
+            } elseif (str_replace(':', '', $gio_thi[0]) < 1800) {
+                $ca_chieu = $gio_thi[0];
+            } else {
+                $ca_toi = $gio_thi[0];
+            }
+        }
+    } elseif (count($gio_thi) == 3) {
+        // echo 'case=3';
+        $ca_sang = $gio_thi[0];
+        $ca_chieu = $gio_thi[2];
+        $ca_toi = 0;
+        // if ($ca_sang == "AM") {
+        //     $ca_sang = "7:00";
+        // }
+        // if ($ca_chieu == "PM") {
+        //     $ca_chieu = "13:00";
+        // }
+    } elseif (count($gio_thi) == 5) {
+        // echo 'case=5';
+        $ca_sang = $gio_thi[0];
 
+        if (str_replace(':', '', $gio_thi[2]) < 1800 && str_replace(':', '', $gio_thi[2]) > 1299) {
+            $ca_chieu =  $gio_thi[2];
+            $ca_toi = $gio_thi[4];
+        } elseif ($gio_thi[4] < 1800) {
+            $ca_chieu =  $gio_thi[4];
+            $ca_toi = 0;
+        }
+
+
+
+
+        if (strpos($gio_thi[0], 'AM') !== false) {
+            $ca_sang = "7:00";
+        }
+        if (strpos($gio_thi[2], 'PM') !== false) {
+            $ca_chieu = "13:00";
+        }
+        if (strpos($gio_thi[4], 'EV') !== false) {
+            $ca_toi = "18:00";
+        }
+    }
+    // echo "sang: " . $ca_sang . "   chieu: " . $ca_chieu . "   toi: " . $ca_toi;
+    // echo "     count:" . count($gio_thi) . "    ";
+    // print_r($gio_thi);
+    // echo "<br>";
     $list_result[] = array(
         "bai_thi" => trim($item->childNodes(1)->plaintext),
         "ngay_thi" => date("Y-m-d", strtotime(str_replace("/", "-", trim($item->childNodes(2)->plaintext)))),
-        // "ngay_thi" => trim($item->childNodes(2)->plaintext),
-        "gio_thi" => trim($item->childNodes(3)->plaintext),
+        "gio_thi_ca_sang" => $ca_sang,
+        "gio_thi_ca_chieu" => $ca_chieu,
+        "gio_thi_ca_toi" => $ca_toi,
         "dia_diem_thi" => trim($item->childNodes(4)->plaintext),
         "ngay_tra_ket_qua" => trim($item->childNodes(5)->plaintext)
     );
     $bai_thi = trim($item->childNodes(1)->plaintext);
     $ngay_thi = date("Y-m-d", strtotime(str_replace("/", "-", trim($item->childNodes(2)->plaintext))));
-    $gio_thi = trim($item->childNodes(3)->plaintext);
+    $gio_thi_ca_sang = $ca_sang;
+    $gio_thi_ca_chieu = $ca_chieu;
+    $gio_thi_ca_toi = $ca_toi;
     $dia_diem_thi = trim($item->childNodes(4)->plaintext);
     $ngay_tra_ket_qua = trim($item->childNodes(5)->plaintext);
-    $ngay_tra_ket_qua = trim($item->childNodes(5)->plaintext);
-    $update_at = date("Y/m/d H:i");
-    $sql = "INSERT INTO lich_thi_va_ket_qua (bai_thi, ngay_thi, gio_thi, dia_diem_thi, ngay_tra_ket_qua, created_at, updated_at)
-    VALUES ('$bai_thi', '$ngay_thi', '$gio_thi', '$dia_diem_thi', '$ngay_tra_ket_qua', '$update_at', '$update_at')";
+    $status = 0;
+    if ($item->hasClass("active")) {
+        $status = 1;
+    }
+    $updated_at = date("Y/m/d H:i");
+    $sql = "INSERT INTO lich_thi_va_ket_qua (bai_thi, ngay_thi, gio_thi_ca_sang, gio_thi_ca_chieu, gio_thi_ca_toi, dia_diem_thi, ngay_tra_ket_qua, status, created_at, updated_at)
+    VALUES ('$bai_thi', '$ngay_thi', '$gio_thi_ca_sang', '$gio_thi_ca_chieu', '$gio_thi_ca_toi', '$dia_diem_thi', '$ngay_tra_ket_qua', '$status', '$updated_at', '$updated_at')";
     if (mysqli_query($conn, $sql) === TRUE) {
         // echo "insert successfull";
     } else {
--- index.php
+++ index.php
@@ -2,8 +2,16 @@
     <button type="submit">Crawl</button>
 </form>
 <?php require_once 'connect.php'; ?>
-
+<style>
+    td {
+        border: 1px solid #000;
+    }
+</style>
 <table>
+    <?php
+
+
+    ?>
     <?php
     $sql = "SELECT * FROM lich_thi_va_ket_qua";
     $result = $conn->query($sql);
@@ -11,11 +19,18 @@
     if ($result->num_rows > 0) {
         // output
         while ($row = $result->fetch_assoc()) {
-            echo '<tr>';
+            if ($row["status"] == 1) {
+                $style = 'style="color: red;"';
+            } else {
+                $style = '';
+            }
+            echo '<tr ' . $style . '>';
             echo '<td>' . $row["id"] . "</td>";
             echo '<td>' . $row["bai_thi"] . "</td>";
             echo '<td>' . date("d-m-Y", strtotime($row["ngay_thi"])) . "</td>";
-            echo '<td>' . $row["gio_thi"] . "</td>";
+            echo '<td>' . $row["gio_thi_ca_sang"] . "</td>";
+            echo '<td>' . $row["gio_thi_ca_chieu"] . "</td>";
+            echo '<td>' . $row["gio_thi_ca_toi"] . "</td>";
             echo '<td>' . $row["dia_diem_thi"] . "</td>";
             echo '<td>' . $row["ngay_tra_ket_qua"] . "</td>";
             echo '</tr>';
